5d066e026788da952456a1068f35be79e7ca4154
DonnyWu <dong.yuen5@gmail.com>
2025-07-09 18:09:07 +0800

    feat: 增修消費者資料

---
+++ database/migrations/2025_07_09_095839_create_consumers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consumers', function (Blueprint $table) {
+            $table->comment('消費者基本資料');
+            $table->id()->comment('資料識別碼');
+            $table->string('name',20)->comment('消費者名稱');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consumers');
+    }
+};
